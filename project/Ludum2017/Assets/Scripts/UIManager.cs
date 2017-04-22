4b9d8c7cf0d607a7a0ffb652cd4f24350b0e6dbb
vacarriean <carriec@gmail.com>
2017-04-22 19:10:04 -0400

    ui scene and management

---
+++ project/Ludum2017/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour 
+{
+	public static UIManager Instance;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	public GameObject currentWindow;
+
+	public void SetNewWindow(GameObject newWindow)
+	{
+		if (currentWindow != null) 
+		{
+			currentWindow.SetActive (false);
+		}
+
+		currentWindow = newWindow;
+		currentWindow.SetActive (true);
+	}
+}
