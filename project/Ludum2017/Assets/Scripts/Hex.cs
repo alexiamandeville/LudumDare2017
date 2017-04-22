1dd419a70f68929290c8206c62ed177f0aa70766
C-Through <drew.l.connell@gmail.com>
2017-04-22 11:04:20 -0400

    Planet, Hex, Raycast for networked player
    Planet that gets vertex/normal data, I still need to put together a
    prefab for the hex space.

---
+++ project/Ludum2017/Assets/Scripts/Hex.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HexType
+{
+    None,
+    
+    Animal,
+    Road,
+    City,
+    River
+}
+
+public class Hex : MonoBehaviour
+{
+    public HexType currentType = HexType.None;
+
+    // Fill the selected hex with the 
+    public void Fill(HexType playerType)
+    {
+        // If the hex already has a type, return
+        if (currentType != HexType.None)
+            return;
+
+        currentType = playerType;
+
+        GetComponentInChildren<MeshRenderer>().material.color = Color.red;
+    }
+
+    public void Clear()
+    {
+        currentType = HexType.None;
+    }
+
+    private void CheckNeighbors()
+    {
+        // When filled, check objects near this one to create special objects
+    }
+}
---
+++ project/Ludum2017/Assets/Scripts/Planet.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public GameObject mCell;
+
+    void Awake()
+    {
+        DrawNormals();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        /*
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawSphere(transform.position, 0.5f);
+    
+        foreach (Vector3 vertexPosition in GetVertices())
+        {
+            // World position
+            Vector3 targetPosition = vertexPosition + transform.position;
+
+            // Factor in rotation to world position
+            targetPosition = transform.rotation * (targetPosition - transform.position) + transform.position;
+        }
+        */
+    }
+
+    private void DrawNormals()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            // Position, rotation
+            Vector3 normal = transform.TransformDirection(mesh.normals[i]);
+            Vector3 vertex = transform.TransformPoint(mesh.vertices[i]);
+
+            // Create object
+            GameObject newObject = Instantiate(mCell, vertex, Quaternion.identity);
+            newObject.transform.up = normal;
+
+            Debug.DrawRay(vertex, normal * 1.0f, Color.red);
+        }
+    }
+
+    private List<Vector3> GetVertices()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+
+        vertices.AddRange(mesh.vertices);
+
+        return vertices;
+    }
+}
